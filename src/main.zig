8d50347ee984dbae6a2c8c08f577e5ee5768a0b2
4zv4l <simon.blacks.be@gmail.com>
2022-07-06 16:17:20 +0200

    show heap usage w and w/ the user input count

--- src/main.zig
+++ src/main.zig
@@ -31,7 +31,8 @@ pub fn exec(cmd: []const u8, hash: *HashMap, allocator: CountingAllocator) !u8 {
     } else if (cmp(cmd, "clear")) {
         std.debug.print("\x1Bc", .{});
     } else if (cmp(cmd, "heap")) {
-        std.debug.print("=> {}\n", .{allocator.allocated_bytes});
+        std.debug.print("wPrompt  => {}\n", .{allocator.allocated_bytes});
+        std.debug.print("w/Prompt => {}\n", .{allocator.allocated_bytes - 4});
     } else if (cmp(cmd[0..3], "add")) {
         if (cmd.len <= 4) {
             return 4;
